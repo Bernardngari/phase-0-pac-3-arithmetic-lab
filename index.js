62025bf4f40c93ef24f3c3cd0c07385543fc1f27
Bernardngari <bernardngari.ke@gmail.com>
2022-06-03 15:28:16 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,61 @@
+function add(){
+  
+}
+
+function subtract(){
+  
+}
+function multiply(){
+  
+}
+
+function divide(){
+  
+}
+
+function add(a,b){
+  return a + b;
+}
+add( 1000 + 151 );
+
+function subtract(a,b){
+  return a - b;
+}
+subtract( 0 - 308 );
+
+function multiply(a,b){
+  return a * b;
+}
+multiply( 1 * 52688 );
+
+function divide(a,b){
+  return a / b;
+}
+
+divide( 0.0625, 1 );
+
+function increment(a){
+  return a + 1;
+}
+increment( 0 );
+
+function decrement(a) {
+  return a - 1;
+}
+
+function makeInt(n){
+  return n.toString();
+}
+
+function makeInt(n){
+  return parseInt(n,10);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(2.222)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n.toString());
+}
+
